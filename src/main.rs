c5fbce311bf53b058808d8ee066c96c1fc3f2a30
Karandeep Nagra <karandeepsnagra@gmail.com>
2025-03-25 21:01:27 -0700

    [WIP] Add cuckoo_hash_table

--- src/main.rs
+++ src/main.rs
@@ -8,11 +8,13 @@ mod lpts_hash_table;
 mod lprh_hash_table;
 mod qpts_hash_table;
 mod dhts_hash_table;
+mod cuckoo_hash_table;
 
 
 fn main() {
     let all_algs = [
         "menu", "kadane", "common_chars", "lpts_hash_table", "lprh_hash_table", "qpts_hash_table",
+        "cuckoo_hash_table",
     ];
     let args: Vec<_> = env::args().collect();
     if args.len() != 2 {
@@ -36,6 +38,8 @@ fn main() {
         qpts_hash_table::run();
     } else if alg_name == "dhts_hash_table" {
         dhts_hash_table::run();
+    } else if alg_name == "cuckoo_hash_table" {
+        cuckoo_hash_table::run();
     } else {
         panic!("Could not find \"{}\" as a run target", alg_name);
     }
